5cf487ff809804aeaae44cfd424bbb5cb11429f8
shadlymaliyekkal <shadlymaliyekkalofficial80@gmail.com>
2026-01-28 16:32:37 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import { Toaster } from "./components/ui/toaster"
-import { Toaster as Sonner } from "./components/ui/sonner"
-import { TooltipProvider } from "./components/ui/tooltip"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { HashRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index"
-import NotFound from "./pages/NotFound"
+import { Toaster } from "./components/ui/toaster";
+import { Toaster as Sonner } from "./components/ui/sonner";
+import { TooltipProvider } from "./components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter>
-  <Routes>
-    <Route path="/" element={<Index />} />
-    <Route path="*" element={<NotFound />} />
-  </Routes>
-</HashRouter>
+      <BrowserRouter basename="/shadly-protofolio">
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+
     </TooltipProvider>
   </QueryClientProvider>
-)
+);
 
-export default App
+export default App;
