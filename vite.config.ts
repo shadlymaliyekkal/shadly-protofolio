ffa61b3cd3760e62b5c515f88bbbc92c4d3cc508
shadlymaliyekkal <shadlymaliyekkalofficial80@gmail.com>
2026-01-28 11:16:27 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import path from "path"
 
 export default defineConfig({
-  base: "/shadly-protofolio/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "index.html"),
-      },
-    },
-  },
+  base: "/shadly-protofolio/",
 })
